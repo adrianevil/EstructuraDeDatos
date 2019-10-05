69106aa2b906e980ec3ae90a55e1b73035a73b73
ADRIAN SEVILLA <adrianevil@gmail.com>
2019-10-05 13:41:33 -0500

    Add files via upload

---
+++ src/estructuradedatos/EstructuraDeDatos.java
@@ -0,0 +1,21 @@
+/*
+ * UNIVERSIDAD JUAREZ AUTONOMA DE TABASCO.
+ * DIVISIÓN ACADEMICA DE INFORMATICA Y SISTEMAS
+ * PROGRAMACION Y ESTRUCTURA DE DATOS
+ */
+package estructuradedatos;
+
+/**
+ *
+ * @author adrianevil
+ */
+public class EstructuraDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
